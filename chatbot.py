3285218ee14c103f7994abfd53370604cbf62b05
Muhammad Usman <usman.aamir404@gmail.com>
2025-01-12 14:19:49 +0500

    Code Updated

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.memory import ChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
 
 load_dotenv()
 
